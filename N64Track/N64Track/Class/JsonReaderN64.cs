8cfc8dcc48ec20e10d21e6602446a657a61d0677
dhlopez <dhelaman@hotmail.com>
2016-11-24 12:01:31 -0500

    Web vs File

--- N64Track/N64Track/Class/JsonReaderN64.cs
+++ N64Track/N64Track/Class/JsonReaderN64.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,18 +19,8 @@ namespace N64Track.Data
 
         public JsonReaderN64()
         {
-            /*
-            List<Game> monkeys;
-            using (var reader = new System.IO.StreamReader(stream))
-            {
-                var serializer = new XmlSerializer(typeof(List<Monkey>));
-                monkeys = (List<Monkey>)serializer.Deserialize(reader);
-            }
-            var listView = new ListView();
-            listView.ItemsSource = monkeys;
-            */
         }
-        public string getData(string s)
+        async public void getData(string s)
         {
             assembly = typeof(Game).GetTypeInfo().Assembly;
             if (s == null)
@@ -37,15 +28,31 @@ namespace N64Track.Data
 
             stream = assembly.GetManifestResourceStream(s);
 
+
             string gameString = "";
 
             using (StreamReader sr = new StreamReader(stream))
             {
-                gameString = sr.ReadToEnd();
+                
+                //web method
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                var address = $"http://zxcv.us/json/n64pal.json"; 
+                var response = await client.GetAsync(address);
+                //file
+                if(s.Equals("N64Track.Data.n64jap.json"))
+                {
+                    gameString = sr.ReadToEnd();
+                }
+                //web
+                if (s.Equals("N64Track.Data.n64pal.json"))
+                {
+                    gameString = response.Content.ReadAsStringAsync().Result;
+                    
+                }
                 JsonTextReader reader = new JsonTextReader(new StringReader(gameString));
                 //var game = JsonConvert.DeserializeObject<Game>(reader.Value);
-
-                string result = "";
+                Game game = new Game();
 
                 while (reader.Read())
                 {
@@ -56,43 +63,71 @@ namespace N64Track.Data
                     else
                     {
                         //build the object
-                        Game game = new Game();
-                        for (int i = 0; i < 9; i++)
-                        {
-                            if (i == 0 && reader.Value != null)
+                        if (reader.Value.Equals("Name"))
+                        { //&& reader.Value != null)
+                            game = new Game();
+                            if (reader.Read() && reader.Value != null)
                                 game.Name = reader.Value.ToString();
-                            if (i == 1 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("Year"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.Year = reader.Value.ToString();
-                            if (i == 2 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("Developer"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.Developer = reader.Value.ToString();
-                            if (i == 3 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("Publisher"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.Publisher = reader.Value.ToString();
-                            if (i == 4 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("Region"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.Region = reader.Value.ToString();
-                            if (i == 5 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("Players"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.Players = reader.Value.ToString();
-                            if (i == 6 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("ESRB"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.ESRB = reader.Value.ToString();
-                            if (i == 7 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("Genre"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.Genre = reader.Value.ToString();
-                            if (i == 8 && reader.Value != null)
+                            continue;
+                        }
+                        if (reader.Value.Equals("URL"))
+                        { //&& reader.Value != null)
+                            if (reader.Read() && reader.Value != null)
                                 game.URL = reader.Value.ToString();
-
-                            //result += reader.Value + " ";
-                            if (i != 9) //dont read if it is the last one
-                                reader.Read();
+                            App.Database.SaveGame(game);
+                            continue;
                         }
                         //for one object
                         //      return result;
-                        App.Database.SaveGame(game);
-
                     }
 
                 }
                 //for all records, getting exception (when using object instead of printing)
                 //return result;
             }
-            return "";
+            //return "";
         }
     }
 }
